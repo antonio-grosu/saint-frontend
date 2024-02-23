f7ef3575922b47ee7654e9b34001c5cca18c50f4
codevs <contact.codevs@gmail.com>
2024-02-23 20:45:49 +0200

    rm hscreen age

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="font bg-pink-500">
-        <Age />
+        {/* <Age /> */}
         <Navbar />
         {children}
         <Footer />
